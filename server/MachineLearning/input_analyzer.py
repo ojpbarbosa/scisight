fedd41076193cac5c85539575f95343436a99bfc
ojpbarbosa <joaopedrofbarbosa@protonmail.com>
2023-10-08 05:13:53 -0300

    refactor(MachineLearning): rename variables

--- server/MachineLearning/input_analyzer.py
+++ server/MachineLearning/input_analyzer.py
@@ -1,7 +1,7 @@
 import spacy
 from spacy.training.example import Example
 import random
-from ml_model import prof_labels, study_labels, train_texts
+from ml_model import occupation_labels, field_labels, train_texts
 from flask import jsonify
 
 
@@ -33,17 +33,17 @@ def train_model(train_texts, train_labels):
 
 
 def setup_model():
-    nlp_study = train_model(train_texts, study_labels)
-    nlp_prof = train_model(train_texts, prof_labels)
-    return nlp_study, nlp_prof
+    nlp_field = train_model(train_texts, field_labels)
+    nlp_occupation = train_model(train_texts, occupation_labels)
+    return nlp_field, nlp_occupation
 
 
-def run(input_query, nlp_study, nlp_prof):
+def run(input_query, nlp_field, nlp_occupation):
     # testing the models
-    doc_study = nlp_study(input_query)
-    doc_prof = nlp_prof(input_query)
+    doc_field = nlp_field(input_query)
+    doc_occupation = nlp_occupation(input_query)
 
     # getting the maximum category for each model
-    max_study_category = get_max_category(doc_study.cats)
-    max_prof_category = get_max_category(doc_prof.cats)
-    return jsonify({"input": input_query, "study": max_study_category, "profession": max_prof_category})
+    max_field_category = get_max_category(doc_field.cats)
+    max_occupation_category = get_max_category(doc_occupation.cats)
+    return jsonify({"input": input_query, "field": max_field_category, "occupation": max_occupation_category})
