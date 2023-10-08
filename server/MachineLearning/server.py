3896021b92ea30bb7bda62270582653eb90868e8
ojpbarbosa <joaopedrofbarbosa@protonmail.com>
2023-10-08 02:22:43 -0300

    chore(MachineLearning): add cors

--- server/MachineLearning/server.py
+++ server/MachineLearning/server.py
@@ -1,26 +1,20 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 from input_analyzer import setup_model, run
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 nlp_study, nlp_prof = setup_model()
 
 
-@app.route('/predict', methods=['POST'])
+@app.route('/api/predict', methods=['POST'])
 def predict():
     try:
         data = request.get_json()
         text = data['text']
         return run(text, nlp_study, nlp_prof)
-    except Exception as e:
-        return jsonify({"error": str(e)})
-
-
-@app.route('/wedidnthostedourserveryet', methods=['POST'])
-def post_to_java_api():
-    try:
-        return jsonify({"message": ""})
     except Exception as e:
         return jsonify({"error": str(e)})
 
