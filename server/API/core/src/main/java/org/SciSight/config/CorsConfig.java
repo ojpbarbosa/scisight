8b41858ccb8ab26cc38a358e316fb15236a96990
vauP <victor.adpg@gmail.com>
2023-10-08 23:04:48 -0200

    chore(corsConfig)

--- server/API/core/src/main/java/org/SciSight/config/CorsConfig.java
+++ server/API/core/src/main/java/org/SciSight/config/CorsConfig.java
@@ -9,9 +9,9 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
       registry.addMapping("/**")
-        .allowedOriginPatterns("http://localhost:3000")
+        .allowedOriginPatterns("*")
         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
         .allowedHeaders("*")
-        .allowCredentials(true);
+        .allowCredentials(false);
     }
 }
