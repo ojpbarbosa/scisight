4ac4c87a96450287cdab7295561a7244f7273cf5
vauP <victor.adpg@gmail.com>
2023-10-08 22:36:23 -0200

    updating cors

--- server/API/core/src/main/java/org/SciSight/config/CorsConfig.java
+++ server/API/core/src/main/java/org/SciSight/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
       registry.addMapping("/**")
-        .allowedOriginPatterns("*")
+        .allowedOriginPatterns("http://localhost:3000")
         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
         .allowedHeaders("*")
         .allowCredentials(true);
