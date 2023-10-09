58cf6c477f354a2da3ef6605571e3aeaa9e0f670
vauP <victor.adpg@gmail.com>
2023-10-08 22:18:13 -0200

    updating cors

--- server/API/core/src/main/java/org/SciSight/config/CorsConfig.java
+++ server/API/core/src/main/java/org/SciSight/config/CorsConfig.java
@@ -1,25 +1,17 @@
 package org.SciSight.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
-
-  @Bean
-  public WebMvcConfigurer corsConfigurer() {
-    return new WebMvcConfigurer() {
-      @Override
-      public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-          .allowedOrigins("*")
-          .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
-          .allowedHeaders("*")
-          .allowCredentials(true);
-      }
-    };
-  }
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+      registry.addMapping("/**")
+        .allowedOriginPatterns("*")
+        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+        .allowedHeaders("*")
+        .allowCredentials(true);
+    }
 }
-
