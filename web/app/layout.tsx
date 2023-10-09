666528e23602053e1d66853a92e1bcda877626d4
ojpbarbosa <joaopedrofbarbosa@protonmail.com>
2023-10-08 23:52:02 -0300

    feat(web): add analytics

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react'
 
 import { Toaster } from '@/components/ui/toaster'
 import Providers from './providers'
@@ -27,6 +28,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           {children}
         </Providers>
         <Toaster />
+        <Analytics />
       </body>
     </html>
   )
